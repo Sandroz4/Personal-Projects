f4c8bf85fdf53d7a015373c4a96275adf95f0872
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-02-19 22:01:15 +0400

    commit

---
+++ Javascript/introduction/script.js
@@ -0,0 +1,38 @@
+// let name = document.getElementById('test');
+// name.textContent = 'tested';
+
+// let newValue = name.textContent + ' value';
+// console.log(newValue);
+
+// let userName = document.getElementById('name').value; 
+// let paragraphElement = document.querySelector('p'); 
+
+// paragraphElement.textContent += ' ' + userName;
+
+// function nameX(){
+//     let name = document.getElementById('name').textContent
+//     console.log(name + ' zabakhidze')
+// } 
+
+// nameX()
+
+// function colorChange(){
+//     let text = document.querySelector('p')
+//     let color = document.querySelector('input').value
+//     text.style.color = color
+// }
+
+// colorChange()
+
+// function widthChange() {
+//     let user = document.querySelector('input').value;
+//     let image = document.querySelector('img'); 
+
+//     image.style.width = user + 'px'; 
+// }
+
+// console.log(5>4)
+// console.log(2>4)
+// console.log(5>=4)
+// console.log(5>=5)
+
